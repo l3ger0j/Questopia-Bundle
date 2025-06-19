84edacc40501387573d24641d9b147686565f071
WhoYouAndM3 <83360444+l3ger0j@users.noreply.github.com>
2025-06-20 00:39:21 +0300

    Bump to `0.4.0`

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "org.qp.android.questopiabundle"
         minSdk = 26
         targetSdk = 35
-        versionCode = 100200
-        versionName = "0.3.0"
+        versionCode = 100300
+        versionName = "0.4.0"
 		
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
